ae339cfdf7efaa3603e2bd15f2d013d7bddff92a
Josh Tuck <josh@rustbelt.co>
2019-02-18 11:54:15 -0500

    Josh finally fixes the vertical pages.
    in this episode, Josh makes numerous CSS and HTML changes and finally fixes the vertical pages.

--- vertical-manufacturing.php
+++ vertical-manufacturing.php
@@ -12,6 +12,7 @@
       </div>
     </div>
   </div>
+  <div class="hdrShape hdrShape--mfcr"></div>
   <div class="content main">
     <div class="intro">
       <div class="wrap">
@@ -81,45 +82,49 @@
         <?php endif; ?>
       </div>
     </div>
-    <div class="result">
-      <div class="wrap">
-        <div class="slides">
-          <div class="slide">
-            <img src="<?php echo get_template_directory_uri() ?>/assets/img/tank.png" class="img" alt="Tank">
-            <div class="main-content">
-              <h2>From tanker trailers to valves, port trucks to pumps, components to fire hose nozzles</h2>
-              <p>We have:</p>
-              <p class="words">
-                <span>Launched a new product AND re-launched the brand</span>
-                <span>Trained distributors and dealers to recommend our client’s brand over the competition</span>
-                <span>Inspired (certified) IS employee compliance via gamification</span>
-                <span>Re-launched a client’s brand and revolutionized its culture</span>
-                <span>Launched a new product before it was produced by utilizing 3D renderings</span>
-                <span>Identified and named product differentiation, leveraging their enormous investment in engineering</span>
-                <span>Re-engaged corporate morale through internal branding</span>
-                <span>Created a brand story book to preserve the culture as founders retired</span>
-                <span>Increased sales through promotion that saw 92% channel participation</span>
-                <span>Promoted an underdog product to prominence</span>
-              </p>
-            </div>
-            <div class="overlay" style="min-height:400px;">
-              <p class="words">
-                <span style="font-size:38px; line-height:38px;">saving 35% in budget and increasing sales 15%</span>
-                <span>44% in sales increase</span>
-                <span>97% compliance</span>
-                <span>now reaching 70% market share</span>
-                <span>top selling product in segment</span>
-                <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                <span>exec trust from 23 to 92%</span>
-                <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-                <span>22% inc in flat market</span>
-                <span>from last place to second in segment</span>
-              </p>
-            </div>
-          </div>
+
+    <div class="resultsManu">
+      <div class="resultsManuImg">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/tank.png" class="img" alt="Tank">
+      </div>
+      <div class="flxWrap flxWrap--resultsMan">
+        <div class="flxBloc flxBloc--resManCont resManCont--top">
+          <h2 class="hdln hdln--ResM">From tanker trailers to valves, port trucks to pumps, components to fire hose nozzles, we have:</h2>
+          <p class="words txt txt--ResMwords">
+            <span>Launched a new product AND re-launched the brand.</span>
+            <span>Trained distributors and dealers to recommend our client’s brand over the competition.</span>
+            <span>Inspired (certified) IS employee compliance via gamification.</span>
+            <span>Re-launched a client’s brand and revolutionized its culture.</span>
+            <span>Launched a new product before it was produced by utilizing 3D renderings.</span>
+            <span>Identified and named product differentiation, leveraging their enormous investment in engineering.</span>
+            <span>Re-engaged corporate morale through internal branding.</span>
+            <span>Created a brand story book to preserve the culture as founders retired.</span>
+            <span>Increased sales through promotion that saw 92% channel participation.</span>
+            <span>Promoted an underdog product to prominence.</span>
+          </p>
+        </div>
+        <div class="flxBloc flxBloc--resManBlank"></div>
+      </div>
+      <div class="flxWrap flxWrap--resultsMan">
+        <div class="flxBloc flxBloc--resManBlank"></div>
+        <div class="flxBloc flxBloc--resManCont resManCont--btm">
+          <h2 class="hdln hdln--ResM hdln--resMbtm">Result:</h2>
+          <p class="words txt txt--ResMwords">
+            <span>Saving 35% in budget and increasing sales 15%.</span>
+            <span>44% in sales increase.</span>
+            <span>97% compliance.</span>
+            <span>Now reaching 70% market share.</span>
+            <span>Top selling product in segment.</span>
+            <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <span>Exec trust from 23 to 92%.</span>
+            <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            <span>22% inc in flat market.</span>
+            <span>From last place to second in segment.</span>
+          </p>
         </div>
       </div>
-    </div>
+    </div> <!-- END Results (New) -->
+
     <div class="segments" style="background-image:url('<?php the_field("audiences_background") ?>');">
       <div class="wrap small">
         <div class="row">
